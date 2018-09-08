d67e72100696a403e3bc48946644bce3cb70b0ac
Mazdak Farrokhzad <twingoow@gmail.com>
2018-09-09 00:05:39 +0200

    cleanup.

--- src/lib.rs
+++ src/lib.rs
@@ -10,29 +10,3 @@ extern crate proptest;
 extern crate itertools;
 
 pub mod command;
-
-use std::io;
-
-fn main() -> io::Result<()> {
-    use std::io::prelude::*;
-
-    let mut input = String::new();
-    let stdin = io::stdin();
-    let mut stdin = stdin.lock();
-
-    let stdout = io::stdout();
-    let mut stdout = stdout.lock();
-
-    loop {
-        write!(stdout, "> ")?;
-        stdout.flush()?;
-
-        input.clear();
-        stdin.read_line(&mut input)?;
-
-        match command::parse(&input) {
-            Ok(cmd) => println!("{:?}", cmd),
-            Err(err) => println!("{}", err),
-        }
-    }
-}
