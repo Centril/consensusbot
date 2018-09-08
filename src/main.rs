6164b4aa3635f2da29e015025b67dcda276c8285
Mazdak Farrokhzad <twingoow@gmail.com>
2018-09-08 15:56:33 +0200

    drop nom parser.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,12 @@
 #![recursion_limit="128"]
 
-#[macro_use] extern crate nom;
-
 #[macro_use] extern crate maplit;
 #[macro_use] extern crate combine;
 
 extern crate itertools;
 
-mod nom_command;
 mod command;
-
-fn parse_vec_ok(text: &str) -> Vec<nom_command::Command<&str>> {
-    nom_command::parse(text)
-        .filter_map(Result::ok)
-        .map(|(_, cmd)| cmd)
-        .collect::<Vec<_>>()
-}
 
 fn main() {
     command::main();
-
-    println!("{:#?}", parse_vec_ok("{{@rfcbot merge}}"));
 }
